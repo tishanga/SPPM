88039177c6bb236a7c16599a634f423fad4a18d9
Stephen Musprett <stephen_1717@LANKACLEAR.LK>
2023-07-16 11:26:21 +0530

    loginandsingup

---
+++ config.php
@@ -0,0 +1,23 @@
+<?php
+
+//$conn = mysqli_connect('localhost','root','','user_db');
+
+// Replace the placeholder values with your actual database credentials
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "user_db";
+
+//  database connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+// Check if the connection was successful
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+
+
+
+
+?>
