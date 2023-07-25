23518f52b64d380332e15e3dfe2fd1b77202edf5
tishanga <67939352+tishanga@users.noreply.github.com>
2023-07-25 21:46:22 +0530

    database

---
+++ user_form.sql
@@ -0,0 +1,71 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 25, 2023 at 05:34 PM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.1.17
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `user_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_form`
+--
+
+CREATE TABLE `user_form` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `user_type` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `user_form`
+--
+
+INSERT INTO `user_form` (`id`, `name`, `email`, `password`, `user_type`) VALUES
+(1, 'admin', 'admin@gmail.com', 'e6e061838856bf47e1de730719fb2609', 'admin'),
+(2, 'Stephen Musprett', 'stephenmusprett@gmail.com', '02de1c0ddfe492248c94f6f9c468cf6f', 'admin'),
+(3, 'Thisanga', 'tisanga@gmail.com', '827ccb0eea8a706c4c34a16891f84e7b', ''),
+(4, 'Akshara Kodithuwakku', 'aksharakodithuwakku97@gmail.com', 'bea5a04cf16a06646326fc25b53db28e', '');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `user_form`
+--
+ALTER TABLE `user_form`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `user_form`
+--
+ALTER TABLE `user_form`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
